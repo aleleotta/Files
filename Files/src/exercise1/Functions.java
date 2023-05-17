f0fc1e9f8694cc9816c052224faa209642130902
Alessandro Leotta <Alessandro Leotta@HPaleleotta>
2023-05-17 13:58:03 +0200

    .

--- Files/src/exercise1/Functions.java
+++ Files/src/exercise1/Functions.java
@@ -2,3 +2,10 @@ package exercise1;
 
 public class Functions {
 }
+
+/*
+Crea con un editor el fichero de texto NumerosReales.txt en la carpeta del proyecto.
+Escribe en él una serie de números reales separados por espacios simples.
+Implementar un programa que acceda a NumerosReales.txt,
+lea los números y calcule la suma y la media aritmética, mostrando los resultados por pantalla.
+*/
--- Files/src/exercise1/Main.java
+++ Files/src/exercise1/Main.java
@@ -7,3 +7,10 @@ public class Main{
 		sc.close();
 	}
 }
+
+/*
+Crea con un editor el fichero de texto NumerosReales.txt en la carpeta del proyecto.
+Escribe en él una serie de números reales separados por espacios simples.
+Implementar un programa que acceda a NumerosReales.txt,
+lea los números y calcule la suma y la media aritmética, mostrando los resultados por pantalla.
+*/
