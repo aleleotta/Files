514ae21e6a40e38f9cfe6d24b01ed76966e684af
Alessandro Leotta <Alessandro Leotta@HPaleleotta>
2023-05-17 14:01:32 +0200

    .

--- Files/src/exercise2/Main.java
+++ Files/src/exercise2/Main.java
@@ -7,3 +7,15 @@ public class Main{
 		sc.close();
 	}
 }
+
+/*
+Crear con un editor el fichero de texto Enteros.txt en la carpeta del proyecto y
+escribe en él una serie de números enteros separados por secuencias de espacios y tabuladores,
+incluso en líneas distintas, tal como:
+
+2	3  45		73
+  123		4   21
+
+Implementar un programa que acceda a Enteros.txt con un objeto Scanner a través de un flujo de entrada,
+lea los números y calcule la suma y la media aritmética, mostrando los resultados por pantalla.
+*/
--- Files/src/exercise3/Main.java
+++ Files/src/exercise3/Main.java
@@ -7,3 +7,16 @@ public class Main{
 		sc.close();
 	}
 }
+
+/*
+Crea con un editor el fichero de texto Alumnos.txt en la carpeta del proyecto y escribe en él los nombres,
+edades y estaturas de los alumnos de un grupo, cada uno en una línea:
+
+	juan 22 1.77
+	luis 21 1.80
+	pedro 20 1.73
+	…
+
+Implementa un programa que lea del fichero los datos,
+muestre los nombres y calcule la media de la edad y de las estaturas, mostrándolas por pantalla.
+*/
