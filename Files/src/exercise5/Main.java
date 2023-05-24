809a9147a22c7d6debdff6a2144a4d369ec7cbe2
Alessandro Leotta <Alessandro Leotta@HPaleleotta>
2023-05-24 20:38:22 +0200

    .

--- Files/src/exercise5/Main.java
+++ Files/src/exercise5/Main.java
@@ -7,3 +7,8 @@ public class Main{
 		sc.close();
 	}
 }
+
+/*
+Diseña una aplicación que pida al usuario su nombre y edad. Estos datos deben guardarse en el fichero datos.txt.
+Si este fichero existe, deben añadirse al final en una nueva línea, y en caso de no existir, debe crearse.
+*/
