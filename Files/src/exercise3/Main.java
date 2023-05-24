5f224519f90b5ab698e2f3721d9db31ec4dd1c46
Alessandro Leotta <Alessandro Leotta@HPaleleotta>
2023-05-24 20:07:45 +0200

    .

--- Files/src/exercise3/Main.java
+++ Files/src/exercise3/Main.java
@@ -13,13 +13,32 @@ public class Main{
 			int age = 0;
 			double height = 0;
 			int counter = 0;
+			int averageAge = 0;
+			double averageHeight = 0;
+			int sumAge = 0;
+			double sumHeight = 0;
 			while(sc.hasNextLine()) {
 				name = sc.next();
 				age = sc.nextInt();
 				height = sc.nextDouble();
 				per = new Student(name, age, height);
+				studentList.add(per);
 				counter++;
 			}
+			for(Student p: studentList) {
+				System.out.println("=====================================================================================");
+				System.out.println("Name: " + p.getName());
+				System.out.println("Age: " + p.getAge());
+				System.out.println("Height: " + p.getHeight());
+				sumAge = sumAge + p.getAge();
+				sumHeight = sumHeight + p.getHeight();
+			}
+			averageAge = sumAge / counter;
+			averageHeight = sumHeight / counter;
+			System.out.println("=====================================================================================");
+			System.out.println("=====================================================================================");
+			System.out.println("Age average: " + averageAge);
+			System.out.println("Height average: " + averageHeight);
 		} catch (FileNotFoundException err) {
 			err.getMessage();
 		} finally {
